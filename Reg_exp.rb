830d63afa7cff5c21310ba2a1d5e9d573d392fde
josephmckenzie <josephmckenzieaz@yahoo.com>
2015-09-25 17:55:54 -0400

    my very first use of a regexp simple as heck but gotta start somewhere

---
+++ Reg_exp.rb
@@ -0,0 +1,7 @@
+# This is the file where im gonna learn how to use regex so i can utilize it in my game and other places........
+# i did a lot of these just using Irb so hopefully it will work just the same once implemented in my code for my adventuregame
+
+#ok here is my first example really simplistic but it satrts with this i guess
+#this one tells us that they both contain the same exact thing 
+/abc/ == /abc/ 
+  
